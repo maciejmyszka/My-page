cd51b798d5c352751d73298831329dec61eed2ad
misz0rr <maciejmyszka99@gmail.com>
2021-09-15 19:10:56 +0200

    adding files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".navbar");
+  const navItems = document.querySelectorAll(".nav-link");
+  const navList = document.querySelector(".navbar-collapse");
+
+  function addShadow() {
+    if (window.scrollY >= 300) {
+      nav.classList.add("shadow-bg");
+    } else {
+      nav.classList.remove("shadow-bg");
+    }
+  }
+
+  navItems.forEach((item) =>
+    item.addEventListener("click", () => navList.classList.remove("show"))
+  );
+
+  window.addEventListener("scroll", addShadow);
+});
